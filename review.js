ab8b37c509b7e946f320b0937c77e712a8100d8e
uzhojiakbar <uzhojiakbar3@gmail.com>
2023-05-02 22:01:30 +0500

    css ni ulash, color, font-size, font-weight, class vs id

---
+++ review.js
@@ -0,0 +1,23 @@
+// CSS ulashni 3 xil usuli bor
+    // 1. Tashqi fayl orqali ulash
+        // <link rel="stylesheet" href="./style.css">
+    // 2. Headni ichida style nomi teg orqali
+        // <style>
+        //     p{
+        //         color: red;
+        //     }
+        // </style>
+    // 3. teg uchun beriladigan Attirubut orqali
+        // <p style="color: red;">Hello</p>
+
+// kodlar tepada pastga oqiladi, va ikkita bir xil xossa kelib qolsa oxirgi kelganini oladi
+        
+// color: blue; - matn rangini ozgartirish
+// font-size: 60px; - shrift kattaligi ({any}px)
+// font-weight: 900; - shrift qalinligi (100-900,bold) bold = 900 
+
+// <p class="hello">Hello</p>
+
+// class vs id
+    // class ga bir necha nom bersak boladi, id ga faqat 1 ta nom beriladi
+    // element xossani class va id teng berilganda id nikini oladi, chunki id gapini otqiza oladi.
