756b24a310f8aba5926e8bd02f99c938bbe2b18a
uzhojiakbar <uzhojiakbar3@gmail.com>
2023-05-03 00:44:11 +0500

    text-align, text-transform, font-family, google fonts, letter-spacing and word-spacing, text-decoration, div vs span, opacity, rgb vs rgba, background-color

---
+++ 2-dars/review.js
@@ -0,0 +1,26 @@
+// text-align - matni satrda qaysi joyida joylashuvini belgilash
+    // left - chap tomonga
+    // right - o'ng tomonga
+    // center - o'rtaga
+    // justify - ikki tomonga tekkislab beradi
+// font-family:; - shriftni sozlash
+    // https://fonts.google.com/
+    // @import url('https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap');
+
+// text-transform:; - harflarni katta-kichikligini sozlash
+    // capitalize - faqatgina bosh harflarni katta harfga o'zgartiradi, qolgan harflar o'z holatida qoladi
+    // lowercase - hammasini kichina qiladi
+    // uppercase - hammasini katta qiladi
+// div vs span
+    // ikkisi ham orab turuvchi
+        // div butun qatorni eggalaydi, span oziga keraklik joyni egalaydi
+// faqatgina a elementi ning ichidagi b elementga style berish
+// letter-spacing: 15px; - harflar orasidan joy tashlash
+// word-spacing: 15px; - so'zlar orasida joy tashlash
+// opacity: 0.8; - shafofligi
+// background-color:; - orqa fon rangi
+// rgb vs rgba
+// text-decoration: 2px underline blue; - 3 ta qiymat, hajmi, turi va rangi
+    // underline - pastga chiziq
+    // overline - tepaga chiziq
+    // line-through - o'rtaga chiziq
