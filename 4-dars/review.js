4670c698989927cc8f8723072a0b06298300e9f6
uzhojiakbar <uzhojiakbar3@gmail.com>
2023-05-09 01:34:29 +0500

    border,border-radius,box-shadow,inset,border-types,display: none; vs visiblity:hidden;, cursor:pointerl

---
+++ 4-dars/review.js
@@ -0,0 +1,45 @@
+// border - eng chekkadidagi chiziq (margin ->  BORDER -> padding -> content)
+// border - 2px solid red;
+    // 3 ta qiymat qabul qiladi, 1-hajmi, 2-turi,3-rangi
+// border types:
+    // solid
+    // dotted
+    // dashed
+    // double
+    // groove
+    // ridge
+    // inset
+    // outset
+    // none
+
+// border-radius: 25px;  - qiyshaytiradi
+// border-top-left-radius:     45px; - tepa chap tomon uchun
+// border-top-right-radius:     45px; - tepa o'ng tomon uchun
+// border-bottom-left-radius:     45px; - past chap tomon uchun
+// border-bottom-right-radius:     45px; - past o'ng tomon uchun
+// width va height bir xil bolganda har tomonga 50% bersak yumoloq holatga keladi
+
+// cursor:pointer; - sichqonchani qol korinishiga olib otadi
+
+// box-shadow: 5px 5px 0 red;
+    // 1-qiymat x oqi
+    // 2-qiymat y oqi
+    // 3-qiymat qancha masofaga yoyilishi
+    // 4-qiymat rangi
+
+//box-shadow:
+    // 5px 5px 0 red,
+    // 10px 10px 0 blue,
+    // 15px 15px 0 yellow,
+    // 20px 20px 0 orange;
+
+// inset - ichgariga soya berish
+// box-shadow: inset 0 0 120px red;
+// box-shadow:
+    // 5px 5px 0 red,
+    // 10px 10px 0 blue,
+    // inset 15px 15px 0 yellow,
+    // 20px 20px 0 orange;
+
+// display:none; - ekrandan oz joyi bilan birga yoqotadi
+// visibility: hidden; - ekrandan yoqotadi lekin joyi qoladi
