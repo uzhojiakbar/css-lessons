2caf7cfa0dd0e5a8d70b9ccb5a31967ffcf7f29d
uzhojiakbar <uzhojiakbar3@gmail.com>
2023-05-12 15:05:54 +0500

    flexbox,justify-content,align-items,flex-firection,gap,background-image repeat and size

---
+++ 5-dars/review.js
@@ -0,0 +1,39 @@
+// box-sizing: border-box; - padding va borderni width ni ichida hisoblaydi
+
+// background-image
+    // background-image: url("./img/back.jpg");
+// background-repeat
+    // repeat - (def) har tomonga takrorlanaversin
+    // repeat-x - x  tomonga takrorlanaversin
+    // repeat-y - y  tomonga takrorlanaversin
+    // no-repeat - umuman takrorlanmasin
+// background-size
+    // contain - sifatini buzilmagan holatda chiroyli qilib qoyish
+    // cover - sifati biroz buzilsa ham, width va height ga to'g'rilansin
+
+// background: url("./img/back.jpg");
+// background-size: cover;
+// background-repeat: no-repeat;
+
+
+// display:flex; - birinchi kegan childini yongan yon joylashtiradi
+// justify-content - x oqi boyicha joylashuvni sozlash
+    // center - elementlar ortaga
+    // space-between - hamma elementlar orasidan bir xilda joy tashlash, chap va ong tomondan tashamaydi
+    // space-around - hamma elementlar orasidan joy tashlaydi, chap va ong tomondan  ortasiga bergan joyni yarmini tashaydi
+    // space-evenly - har tomondan bir xilda joy tashlaydi
+    // (def) left - chap tomondan
+    // right - ong tomondan
+
+// align-items - y oqi boyicha joylashuv
+    // (def) flex-start - flex boshlangan joydan
+    // center - ortadan
+    // flex-end - flex tugagan joydan
+    
+// gap - elementlar orasidan joy tashlash, chap va ong tomondan joy tashamaydi
+
+// flex-direction - flex oqini sozlash
+    //  (def) row - x oqi boyicha yonma-yon togri tartibda joylash 
+    //  row-reverse - x oqi boyicha yonma-yon teskari tartibda joylash 
+    //  column - y oqi boyicha ustunma-ustun togri tartibda joylash 
+    //  column-reverse - y oqi boyicha ustunma-ustun teskari tartibda joylash 
